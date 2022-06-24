513393144b2074b56635b76bbdd4343c439b0d26
Sachin Khatak <khataksachin00@gmail.com>
2022-06-24 19:47:16 +0530

    Readme Commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
 const newYears = '1 Jan 2023';
+
 function countdown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
@@ -14,7 +16,7 @@ function countdown(){
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
-    
+
     daysEl.innerHTML = days;
     hoursEl.innerHTML = hours;
     minsEl.innerHTML = mins;
